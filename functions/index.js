ed97dc244eaffca334f05ce6a4cbb03ec2c08f6a
gagfe14 <gagfe14@gmail.com>
2020-03-05 23:50:02 -0600

    primer commit

--- functions/index.js
+++ functions/index.js
@@ -12,26 +12,27 @@ exports.saludo = functions.https.onRequest(
   (request, response) => {
     try {
       // verifica que el parámetro nombre1 recibido del navegador esté correcto.
+      
       if (!request.query.nombre) {
         // Entra aquí si el nombre es null, undefined o ""
         throw new Error("Se requiere llenar el campo del nombre");
-      
-        } else if (!request.query.apellido) {
+        } 
+
+        else if (!request.query.apellido) {
         // Entra aquí si el apellido es null, undefined o ""
         throw new Error("Se requiere llenar el campo del apellido");
-    
-        } else if (!request.query.correo) {
+        }
+
+        else if (!request.query.correo) {
         // Entra aquí si el correo es null, indefinido o ""
         throw new Error("Se requiere llenar el campo del correo electronico");
-      }
+        }
       /* Solo se llega a esta parte si el nombre, apellido y correo electronico tienen un texto.
        * Devuelve un saludo. */
       response.send(
-        `Datos capturados con exito: 
-        Nombre: ${request.query.nombre} 
-        Apellido ${request.query.apellido}
-        Correo ${request.query.correo}`);
-    } catch (e) {
+        `Datos capturados con exito: Nombre: ${request.query.nombre} Apellido ${request.query.apellido} Correo ${request.query.correo}`);
+    }
+     catch (e) {
       // Devuelve un texto de error.
       response.send(e.message);
     }
